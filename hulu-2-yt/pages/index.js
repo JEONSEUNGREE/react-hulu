822c764c11a75b1861d907c252a53768ed3c03d9
JEONSEUNGREE <jws08219@naver.com>
2022-06-28 17:16:56 +0900

    nav

--- hulu-2-yt/pages/index.js
+++ hulu-2-yt/pages/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../util/request";
 
-export default function Home() {
+export default function Home({results}) {
+
   return (
     <div>
       <Head>
@@ -13,9 +17,27 @@ export default function Home() {
       <Header />
 
       {/* Nav  */}
-    
-      {/* Results */}
+      <Nav />
 
+      {/* Results */}
+      <Results results={results}/>
     </div>
   );
+}
+
+// nextJs 함수 getServerSideProps 매요청마다 서버의 특정동작
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTranding.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
